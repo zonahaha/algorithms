0a54fb0a5f38efce1dfe0d2edc92c1759561da1c
zonahaha <katherjing@gmail.com>
2018-09-08 17:49:11 +0800

    但是只过了10%,提示数组越界访问

---
+++ src/网易第二题.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdio>
+#include <stdlib.h>
+#include <cstring>
+#include <string>
+#include <map>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+
+int getdown(int n,int m){
+
+    int res=0;
+    vector<vector<bool>> v(n);
+    for(int i=0;i<n;i++){
+        v[i].resize(m);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            v[i][j]=0;
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            v[i][j]=!v[i][j];
+            if((j-1>=0)&&(i-1>=0))v[i-1][j-1]=!v[i-1][j-1];//左上
+            if(i-1>=0)v[i-1][j]=!v[i-1][j];//上
+            if((j+1)<m&&(i-1)>=0)v[i-1][j+1]=!v[i-1][j+1];//右上
+            if(j-1>=0)v[i][j-1]=!v[i][j-1];//左
+            if(j+1<m)v[i][j+1]=!v[i][j+1];//右
+            if(i+1<n)v[i+1][j]=!v[i+1][j];//右
+            if((i+1)<n&&(j+1)<m)v[i+1][j+1]=!v[i+1][j+1];//右下
+            if((i+1)<n&&(j-1)>=0)v[i+1][j-1]=!v[i+1][j-1];//左下
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(v[i][j])res++;
+        }
+    }
+
+    return res;
+}
+
+int main(){
+  int t;
+  cin>>t;
+  int m,n;
+  vector<int> res;
+  while(t){
+      cin>>n>>m;
+      res.push_back(getdown(n,m));
+      t--;
+  }
+  for(auto i:res)cout<<i<<endl;
+   return 0;
+}
